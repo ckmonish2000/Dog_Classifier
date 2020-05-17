82b19301de68a9a0885c351f399c80d46453bfdc
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-17 19:52:15 +0530

    first commit

---
+++ classify.py
@@ -0,0 +1,28 @@
+from os import listdir
+import classifier
+path='pet_images/'
+
+
+# print(listdir(path))
+with open('dognames.txt','r') as f:
+    file=f.read()
+    dognames=file.split()  
+# print(dognames)
+
+model='vgg'
+files=listdir(path)
+
+for i in files:
+    result=classifier(f'{path}i',model)
+    print(f"the result for {i} is {result}")
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,13 @@
+from os import listdir
+
+
+filename_list = listdir("pet_images/")
+    
+    # Print 10 of the filenames from folder pet_images/
+# print("\nPrints 10 filenames from folder pet_images/")
+# for idx in range(0, 10, 1):
+#     print("%2d file: %-25s" % (idx + 1, filename_list[idx]))
+
+for i in range(0,10,2):
+    print(i)
+
